c39fdf791678308e8dc849969ba9f4497a0351e9
artemlopatin <artlopatin@gmail.com>
2019-08-25 22:57:23 +0300

    add enum finger

---
+++ src/scenes/KeyboardScene/KeyboardButton/enums.ts
@@ -0,0 +1,11 @@
+export enum finger {
+    'leftPinky',
+    'leftRing',
+    'leftMiddle',
+    'leftIndex',
+    'thumb',
+    'rightIndex',
+    'rightMiddle',
+    'rightRing',
+    'rightPinky',
+}
